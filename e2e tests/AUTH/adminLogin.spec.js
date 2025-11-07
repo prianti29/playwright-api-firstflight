c8682068a2fba2362740744bd1d49396bd0c1b16
Prianti Banik <59369461+prianti29@users.noreply.github.com>
2025-11-07 12:08:07 +0600

    Refactor admin login tests and update fixture data; remove obsolete example test file

--- e2e tests/AUTH/adminLogin.spec.js
+++ e2e tests/AUTH/adminLogin.spec.js
@@ -1,27 +1,29 @@
 import { test, expect } from "@playwright/test";
 import { ADMIN_LOGIN } from "../../support/apiConstants.js";
-import { fixtureData } from "../../fixtures/adminLoginData.json";
-import { superAdminLogin } from "../../support/command.js";
+import { super_admin_login } from "../../support/command.js";
+
+const fixtureData = require("../../fixtures/adminLoginData.json");
 
 const config = require("../../playwright.config.js");
 const BASE_URL = config.use?.BASE_URL;
 
 test.describe("Admin Login Tests", () => {
-  test.beforeAll(async ({ request }) => {
-    await superAdminLogin(request, BASE_URL, ADMIN_LOGIN);
+
+  // 1.1
+  test("Valid Email and Password", async ({ request }) => {
+    await super_admin_login(request, BASE_URL);
   });
 
+  // 1.2
   test("Valid Email and invalid size of Password", async ({ request }) => {
     const Data = fixtureData.jsonData[1];
+
     const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
-      data: {
-        email: Data.email,
-        password: Data.password,
-      },
+      data: Data,
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`,
-      },
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
     });
     const body = await response.json();
     expect(response.status()).toBe(400);
@@ -31,5 +33,66 @@ test.describe("Admin Login Tests", () => {
     );
     expect(body).toHaveProperty("error", "Bad Request");
     expect(body).toHaveProperty("statusCode", 400);
+  });
+
+  // 1.3
+  test("Sign in using invalid credentials", async ({ request }) => {
+    const Data = fixtureData.jsonData[2];
+
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: Data,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    console.log('Response body:', responseBody);
+    expect(response.status()).toBe(401);
+    expect(responseBody.message).toBe(
+      "Incorrect email or password"
+    );
+    expect(responseBody).toHaveProperty("error", "Unauthorized");
+    expect(responseBody).toHaveProperty("statusCode", 401);
+  });
+
+  // 1.4
+  test("Invalid email and valid password", async ({ request }) => {
+    const Data = fixtureData.jsonData[3];
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: Data,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    console.log('Response body:', responseBody);
+    expect(response.status()).toBe(400);
+    expect(Array.isArray(responseBody.message)).toBe(true);
+    expect(responseBody.message[0]).toBe(
+      "email must be an email"
+    );
+    expect(responseBody).toHaveProperty("error", "Bad Request");
+    expect(responseBody).toHaveProperty("statusCode", 400);
+  });
+
+  // 1.5
+  test("Valid email end incorrect password", async ({ request }) => {
+    const Data = fixtureData.jsonData[4];
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: Data,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    expect(response.status()).toBe(401);
+    expect(responseBody.message).toBe(
+      "Incorrect email or password"
+    );
+    expect(responseBody).toHaveProperty("error", "Unauthorized");
+    expect(responseBody).toHaveProperty("statusCode", 401);
   });
 });
--- support/command.js
+++ support/command.js
@@ -1,64 +1,34 @@
 import { test as base, expect } from '@playwright/test';
-// import { ADMIN_LOGIN } from '../support/apiConstants.js';
-// import { fixtureData } from '../fixtures/adminLoginData.json';
+import { ADMIN_LOGIN } from '../support/apiConstants.js';
+import { fixtureData } from '../fixtures/adminLoginData.json';
 
 const config = require('../playwright.config.js');
 const BASE_URL = config.use?.BASE_URL;
 
-// export const test = base.extend({
-//   superAdminSignIn: async ({ request }, use) => {
-//     const signIn = async (userData = fixtureData.jsonData[0]) => {
-//       const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
-//         data: userData,
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       });
-//       const responseData = await response.json();
-//       expect(response.status()).toBe(200);
-//       expect(responseData).toHaveProperty('accessToken');
-//       expect(responseData).toHaveProperty('searchToken');
 
-//       const superAdminAccessToken = responseData.accessToken;
-//       return { response, superAdminAccessToken};
-//     };
-//     await use({ signIn });
-//   },
-// });
-
-// export { expect };
-
-
-async function superAdminLogin(request, baseUrl, adminLoginEndpoint) {
+async function super_admin_login(request, baseUrl) {
 
   const adminLoginData = require('../fixtures/adminLoginData.json');
   const data = adminLoginData.jsonData[0];
   console.log('Admin Login Data:', data);
-  const apiUrl = `${baseUrl}${adminLoginEndpoint}`;
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-
-  const response = await request.post(apiUrl, {
-    headers,
+  const response = await request.post(`${baseUrl}${ADMIN_LOGIN}`, {
     data: {
       email: data.email,
       password: data.password,
     },
-    failOnStatusCode: false,
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
 
-  // Assertions
-  // expect(response.status()).toBe(200);
   const responseBody = await response.json();
   console.log('Response body:', responseBody);
   expect(responseBody).toHaveProperty('accessToken');
   expect(responseBody).toHaveProperty('searchToken');
 
-  // Store the access token
   const superAdminAccessToken = responseBody.accessToken;
   process.env.SUPER_ADMIN_ACCESS_TOKEN = superAdminAccessToken;
-  return superAdminAccessToken; 
+  return superAdminAccessToken;
 }
 
-module.exports = { superAdminLogin };
+module.exports = { super_admin_login };
