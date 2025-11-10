bc84ac49688f190c7def20e1698af6ee4377d5ac
Prianti Banik <59369461+prianti29@users.noreply.github.com>
2025-11-10 10:53:06 +0600

    Enhance admin login tests with additional scenarios and update fixture data for improved validation; include tests for missing fields and SQL injection handling.

--- e2e tests/AUTH/adminLogin.spec.js
+++ e2e tests/AUTH/adminLogin.spec.js
@@ -25,14 +25,15 @@ test.describe("Admin Login Tests", () => {
         Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
       }
     });
-    const body = await response.json();
+    const responseBody = await response.json();
     expect(response.status()).toBe(400);
-    expect(Array.isArray(body.message)).toBe(true);
-    expect(body.message[0]).toBe(
-      "password must be longer than or equal to 6 characters"
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: expect.arrayContaining(["password must be longer than or equal to 6 characters"]),
+        error: "Bad Request",
+        statusCode: 400,
+      })
     );
-    expect(body).toHaveProperty("error", "Bad Request");
-    expect(body).toHaveProperty("statusCode", 400);
   });
 
   // 1.3
@@ -47,13 +48,14 @@ test.describe("Admin Login Tests", () => {
       }
     });
     const responseBody = await response.json();
-    console.log('Response body:', responseBody);
     expect(response.status()).toBe(401);
-    expect(responseBody.message).toBe(
-      "Incorrect email or password"
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: "Incorrect email or password",
+        error: "Unauthorized",
+        statusCode: 401,
+      })
     );
-    expect(responseBody).toHaveProperty("error", "Unauthorized");
-    expect(responseBody).toHaveProperty("statusCode", 401);
   });
 
   // 1.4
@@ -67,14 +69,14 @@ test.describe("Admin Login Tests", () => {
       }
     });
     const responseBody = await response.json();
-    console.log('Response body:', responseBody);
     expect(response.status()).toBe(400);
-    expect(Array.isArray(responseBody.message)).toBe(true);
-    expect(responseBody.message[0]).toBe(
-      "email must be an email"
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: expect.arrayContaining(["email must be an email"]),
+        error: "Bad Request",
+        statusCode: 400,
+      })
     );
-    expect(responseBody).toHaveProperty("error", "Bad Request");
-    expect(responseBody).toHaveProperty("statusCode", 400);
   });
 
   // 1.5
@@ -89,10 +91,113 @@ test.describe("Admin Login Tests", () => {
     });
     const responseBody = await response.json();
     expect(response.status()).toBe(401);
-    expect(responseBody.message).toBe(
-      "Incorrect email or password"
-    );
-    expect(responseBody).toHaveProperty("error", "Unauthorized");
-    expect(responseBody).toHaveProperty("statusCode", 401);
+    expect(responseBody).toMatchObject({
+      message: "Incorrect email or password",
+      error: "Unauthorized",
+      statusCode: 401,
+    });
   });
-});
+
+  // 1.6
+  test("email missing field", async ({ request }) => {
+    const Data = fixtureData.jsonData[5];
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: Data,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    expect(response.status()).toBe(400);
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: expect.arrayContaining([
+          "email must be an email",
+          "email must be a string",
+          "email should not be empty",
+        ]),
+        error: "Bad Request",
+        statusCode: 400,
+      })
+    );
+    expect(responseBody.message).toHaveLength(3);
+  });
+
+  // 1.7
+  test("Password missing field", async ({ request }) => {
+    const Data = fixtureData.jsonData[6];
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: Data,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    expect(response.status()).toBe(400);
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: expect.arrayContaining([
+          "password must be shorter than or equal to 100 characters",
+          "password must be longer than or equal to 6 characters",
+          "password must be a string",
+          "password should not be empty"
+        ]),
+        error: "Bad Request",
+        statusCode: 400,
+      })
+    );
+  });
+
+  // 1.8
+  test("Empty request body", async ({ request }) => {
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: {},
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    expect(response.status()).toBe(400);
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: expect.arrayContaining([
+          "email must be an email",
+          "email must be a string",
+          "email should not be empty",
+          "password must be shorter than or equal to 100 characters",
+          "password must be longer than or equal to 6 characters",
+          "password must be a string",
+          "password should not be empty"
+        ]),
+        error: "Bad Request",
+        statusCode: 400,
+      })
+    );
+  });
+
+  // 1.9
+  test("SQL Injection or Malicious Input", async ({ request }) => {
+    const Data = fixtureData.jsonData[7];
+    const response = await request.post(`${BASE_URL}${ADMIN_LOGIN}`, {
+      data: Data,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.SUPER_ADMIN_ACCESS_TOKEN}`
+      }
+    });
+    const responseBody = await response.json();
+    expect(response.status()).toBe(400);
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        message: expect.arrayContaining([
+          "email must be an email",
+        ]),
+        error: "Bad Request",
+        statusCode: 400,
+      })
+    );
+  });
+}); 
